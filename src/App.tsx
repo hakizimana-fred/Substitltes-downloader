768f3076276e4ede7755db40c0001a0fe448b080
hakifred <hakifred20@gmail.com>
2022-10-22 15:04:46 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
+
+function App () {
+  const [search, setSearch] = useState<string>('')
+
+  useEffect(() => {
+    async function searchSubtitle(): Promise<any>  {
+      try{ 
+      const { data }  = await axios.get('https://api.opensubtitles.com/api/v1/subtitles?query=The Lost City')
+      console.log(data)
+      }catch(err) {
+        console.log(err)
+      }
+    }
+
+    searchSubtitle()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>Hello Application</div>
+  )
+  
+};
 
 export default App;
